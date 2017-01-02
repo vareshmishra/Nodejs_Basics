001feca05305d2ae3f2fddccd163ff6c2a26290a
Varesh Mishra <vareshmishra@gmail.com>
2017-01-02 12:40:37 +0530

    Initialize Repository

---
+++ hello.js
@@ -0,0 +1,10 @@
+var http = require('http'),
+    host = '127.0.0.1',
+    port = '9000';
+
+var server = http.createServer(function(req, res){
+    res.writeHead(200, { 'Content-Type' : 'text/html'});
+    res.end('<h1>Hello World !</h1>');
+}).listen(port, host, function(){
+    console.log("Server running at http://" + host + ":" + port);
+});
